04530143af0454d206b220a7a001728c1c8160c1
Jonas-W <33627730+JHavstein@users.noreply.github.com>
2024-09-09 12:17:36 +0200

    Remove possibility of `build-args`

--- scripts/build-docker-image-2.py
+++ scripts/build-docker-image-2.py
@@ -12,7 +12,6 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Build course Docker image.')
     parser.add_argument('-n', '--image_name', type=str, help='Course name.', required=True)
     parser.add_argument('-d', '--dockerfile_path', type=str, help='Directory of Dockerfile.', required=True)
-    parser.add_argument('-b', '--build_args', type=str, help="Official course code (from university course description)", required=True)
     return parser.parse_args()
 
 def parse_n(arg_n):
@@ -20,17 +19,6 @@ def parse_n(arg_n):
 
 def parse_d(arg_d):
     return(os.path.exists(arg_d) and os.path.basename(arg_d) == "Dockerfile")
-
-def parse_b(arg_b):
-    if len(arg_b) > 0:
-        # Convert to dictionary
-        arg_b = arg_b.strip()
-        arg_b = re.sub('\s*=\s*', '=', arg_b)
-        arg_b = re.sub('\s+', ';',arg_b)
-        arg_b_dict = dict(a.split('=') for a in arg_b.split(';'))
-        return(arg_b_dict)
-    else: 
-        return(arg_b)
 
 if __name__ == "__main__":
     try:
@@ -44,6 +32,6 @@ if __name__ == "__main__":
      
     print("Starting build of %s%s."%(args.dockerfile_path, " with the provided BUILD_ARGS" if len(arg_b_parsed) > 0 else ''))
     print('BE PATIENT ... Building the image may take a while.')
-    client.images.build(path = os.path.dirname(args.dockerfile_path), rm = True, tag = args.image_name, buildargs = arg_b_parsed)
+    client.images.build(path = os.path.dirname(args.dockerfile_path), rm = True, tag = args.image_name)
     print("Building complete. The image '%s' and available under 'Images' in Docker Desktop."%(args.image_name))
     print("Use 'python3 run.py' to start the container.")
