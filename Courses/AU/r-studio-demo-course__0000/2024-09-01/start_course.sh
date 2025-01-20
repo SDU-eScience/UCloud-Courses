3a9988e4441d248550bc5b0c7dc27b94daccf76d
Jonas-W <33627730+JHavstein@users.noreply.github.com>
2025-01-20 16:16:26 +0100

    Minor changes

--- Courses/AU/r-studio-demo-course__0000/2024-09-01/start_course.sh
+++ Courses/AU/r-studio-demo-course__0000/2024-09-01/start_course.sh
@@ -38,61 +38,57 @@ if [[ -f "${INITIALIZATION}" ]]; then
 fi
 
 # Handle class module logic
-if [[ -z "${CLASS}" ]]; then
-    printf "\nNo class selected. Starting RStudio...\n"
-else
-    if [[ ! -d "${PWD}/${CLASS}" ]]; then
-        # Class module selected but folder not mounted
-        printf "\n======================\n"
-        printf "Starting class module\n"
-        printf "======================\n\n"
+if [[ ! -d "${PWD}/${CLASS}" ]]; then
+    # Class module selected but folder not mounted
+    printf "\n======================\n"
+    printf "Starting class module\n"
+    printf "======================\n\n"
 
-        # Download course materials
-        wget "${EXTERNAL_REPO_URL}/contents/classes/${CLASS}" -O "${CLASS}.json" || exit_err "Failed to fetch course materials for ${CLASS}"
-        
-        if [[ -f "${CLASS}.json" ]]; then
-            URLS=$(jq -r '.[].download_url // empty' "${CLASS}.json")
+    # Download course materials
+    wget "${EXTERNAL_REPO_URL}/contents/classes/${CLASS}" -O "${CLASS}.json" || exit_err "Failed to fetch course materials for ${CLASS}"
+    
+    if [[ -f "${CLASS}.json" ]]; then
+        URLS=$(jq -r '.[].download_url // empty' "${CLASS}.json")
 
-            mkdir -p "${PWD}/${CLASS}" || exit_err "Failed to create directory /${PWD}/${CLASS}"
+        mkdir -p "${PWD}/${CLASS}" || exit_err "Failed to create directory /${PWD}/${CLASS}"
 
-            for url in ${URLS}; do
-                if [[ -n "${url}" ]]; then
-                    file_name=$(basename "${url}")
-                    curl -L "${url}" -o "${PWD}/${CLASS}/${file_name}" || exit_err "Failed to download file ${file_name}"
-                    printf "Downloaded file at ${PWD}/${CLASS}/${file_name}\n"
-                else
-                    exit_err "Error: Null or empty URL found in the course materials."
-                fi
-            done
-            rm "${CLASS}.json"
-        else
-            exit_err "Error: Could not find course materials for class module \"${CLASS}\"."
-        fi
-    elif [[ "${REDOWNLOAD}" = true ]]; then
-        # Class module selected and folder mounted but redownload is true
-        printf "\nRedownloading course materials for class module \"${CLASS}\"...\n"
-        wget "${EXTERNAL_REPO_URL}/contents/classes/${CLASS}" -O "${CLASS}.json" || exit_err "Failed to fetch course materials for ${CLASS}"
-
-        if [[ -f "${CLASS}.json" ]]; then
-            URLS=$(jq -r '.[].download_url // empty' "${CLASS}.json")
-
-            for url in ${URLS}; do
-                if [[ -n "${url}" ]]; then
-                    file_name=$(basename "${url}")
-                    curl -L "${url}" -o "${PWD}/${CLASS}/${file_name}" || exit_err "Failed to download file ${file_name}"
-                    printf "Downloaded file at ${PWD}/${CLASS}/${file_name}\n"
-                else
-                    exit_err "Error: Null or empty URL found in the course materials."
-                fi
-            done
-            rm "${CLASS}.json"
-        else
-            exit_err "Error: Could not find course materials for class module \"${CLASS}\"."
-        fi
+        for url in ${URLS}; do
+            if [[ -n "${url}" ]]; then
+                file_name=$(basename "${url}")
+                curl -L "${url}" -o "${PWD}/${CLASS}/${file_name}" || exit_err "Failed to download file ${file_name}"
+                printf "Downloaded file at ${PWD}/${CLASS}/${file_name}\n"
+            else
+                exit_err "Error: Null or empty URL found in the course materials."
+            fi
+        done
+        rm "${CLASS}.json"
     else
-        # Class module selected and folder already mounted, no redownload
-        printf "\nClass module \"${CLASS}\" is already mounted. Skipping download.\n"
+        exit_err "Error: Could not find course materials for class module \"${CLASS}\"."
     fi
+elif [[ "${REDOWNLOAD}" = true ]]; then
+    # Class module selected and folder mounted but redownload is true
+    printf "\nRedownloading course materials for class module \"${CLASS}\"...\n"
+    wget "${EXTERNAL_REPO_URL}/contents/classes/${CLASS}" -O "${CLASS}.json" || exit_err "Failed to fetch course materials for ${CLASS}"
+
+    if [[ -f "${CLASS}.json" ]]; then
+        URLS=$(jq -r '.[].download_url // empty' "${CLASS}.json")
+
+        for url in ${URLS}; do
+            if [[ -n "${url}" ]]; then
+                file_name=$(basename "${url}")
+                curl -L "${url}" -o "${PWD}/${CLASS}/${file_name}" || exit_err "Failed to download file ${file_name}"
+                printf "Downloaded file at ${PWD}/${CLASS}/${file_name}\n"
+            else
+                exit_err "Error: Null or empty URL found in the course materials."
+            fi
+        done
+        rm "${CLASS}.json"
+    else
+        exit_err "Error: Could not find course materials for class module \"${CLASS}\"."
+    fi
+else
+    # Class module selected and folder already mounted, no redownload
+    printf "\nClass module \"${CLASS}\" is already mounted. Skipping download.\n"
 fi
 
 printf "\n===================\n"
