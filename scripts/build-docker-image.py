8df5adb28c1f55ac8cc3333bdc810ff4bacaec8f
Jonas W. <33627730+JHavstein@users.noreply.github.com>
2025-06-10 13:57:01 +0200

    Close #27

--- scripts/build-docker-image.py
+++ scripts/build-docker-image.py
@@ -1,7 +1,7 @@
 __maintainer__ = "SDU eScience Center"
 __email__ = "support@escience.sdu.dk"
 
-import os 
+import os
 import argparse 
 import docker 
 import re 
@@ -18,12 +18,6 @@ def parse_arguments():
     parser.add_argument('-d', '--dockerfile_path', type=str, help='Directory of Dockerfile.', required=True)
     return parser.parse_args()
 
-def parse_n(arg_n):
-    return(arg_n.startswith('dreg.cloud.sdu.dk/ucloud-courses/'))
-
-def parse_d(arg_d):
-    return(os.path.exists(arg_d) and os.path.basename(arg_d) == "Dockerfile")
-
 if __name__ == "__main__":
     try:
         client = docker.from_env()
@@ -32,9 +26,12 @@ if __name__ == "__main__":
         sys.exit(1)  # Exit the script if Docker client cannot be initialized
 
     args = parse_arguments()
-     
+
+    # Get normalized path from -d
+    dockerfile_dir = os.path.dirname(os.path.normpath(args.dockerfile_path))
+         
     print("[INFO] Starting build of %s."%(args.dockerfile_path))
     print('[INFO] BE PATIENT ... Building the image may take a while.')
-    client.images.build(path = os.path.dirname(args.dockerfile_path), rm = True, tag = args.image_name)
+    client.images.build(path = dockerfile_dir, rm = True, tag = args.image_name)
     print("[INFO] Building complete. The image '%s' and available under 'Images' in Docker Desktop."%(args.image_name))
     print("[INFO] Use 'python3 docker-run.py' to start a container.")
--- scripts/create-new-course.py
+++ scripts/create-new-course.py
@@ -18,15 +18,16 @@ import datetime as dt   # For fetching the current month and year. Part of the s
 import requests         # Portable library for making HTTP requests. Part of the standard library
 import git              # For handling paths relative to root dir of repo 
 
-def join_paths(abspath_head, tail):
+def join_paths(head, tail):
     """
-    Joins and creates absolute path /abspath_head/tail.
+    Joins and creates path head/tail and returns path relative to current working direcoty.
+    Head can be both an absolute and relative path. 
 
-    @param abspath_head: The absolute path include parent folder of tail.
+    @param head: The absolute or relative parent directory of tail.
     @param tail: The folder/file name to be appended to abspath_head.
-    @return: The absolute path /abspath_head/tail.
+    @return: The the path head/tail relative to current working directory.
     """
-    return os.path.abspath(os.path.join(abspath_head, tail))
+    return os.path.relpath(os.path.join(head, tail), get_cwd())
 
 def get_cwd():
     """
@@ -194,7 +195,7 @@ if __name__ == "__main__":
             if (not "create-new-course.py" in files_in_cwd) or (not "templates" in files_in_cwd):
                 raise OSError("[ERROR] Current working directory ({}) is incorrect.\nYou must be in UCloud-Courses/scripts.".format(cwd))
             if (not path_tail == "scripts") or (not path_tail_of_head == "UCloud-Courses"):
-                raise OSError("[ERROR]Current working directory ({}) is incorrect.\nYou must be in UCloud-Courses/scripts.".format(cwd))
+                raise OSError("[ERROR] Current working directory ({}) is incorrect.\nYou must be in UCloud-Courses/scripts.".format(cwd))
         except OSError as e:
                 exit(str(e))
     
@@ -213,6 +214,7 @@ if __name__ == "__main__":
 
         course_name = "{}__{}".format(args.name, args.coursecode)
         course_root_dir = os.path.abspath(os.path.join(os.path.split(cwd)[0], 'Courses', args.university.upper(), course_name))
+        course_root_dir = os.path.relpath(course_root_dir, cwd)
         course_release_dir = join_paths(course_root_dir, args.release)
         course_logo_dir = join_paths(course_root_dir, 'logo')
         course_test_dir = join_paths(course_root_dir, 'test')
@@ -237,7 +239,6 @@ if __name__ == "__main__":
 
         # Populate the course folders
         templates_dir = join_paths(cwd, 'templates')
-
         template_readme = join_paths(templates_dir, 'README.md.template') 
         template_dockerfile = join_paths(templates_dir, 'Dockerfile.%s_template'%(args.baseimage))
         template_appyml = join_paths(templates_dir, 'template-app.yml')
@@ -252,7 +253,7 @@ if __name__ == "__main__":
         baseimage_tag = get_baseimage_newest_tag(args.baseimage)
 
         # Get dir of Dockerfile relative to repo root 
-        dockerfile_dir = os.path.relpath(os.path.join(course_release_dir, 'Dockerfile'), start = get_cwd())
+        dockerfile_dir = "../Courses/{}/{}/{}/Dockerfile".format(args.university.upper(), course_name.lower(), args.release)
 
         # Get name of course's Docker image
         courseimage_name = "dreg.cloud.sdu.dk/ucloud-courses/%s_%s:%s"%(args.university, course_name.lower(), args.release)
@@ -306,7 +307,7 @@ if __name__ == "__main__":
 
         # Edit docker-build.py (f6)
         buildimage = re.sub("_IMAGE_NAME", courseimage_name, buildimage)
-        buildimage = re.sub("_DOCKERFILE_DIR", repr(dockerfile_dir), buildimage)
+        buildimage = re.sub("_DOCKERFILE_DIR", dockerfile_dir, buildimage)
 
         # Edit docker-run.template (f7)
         runcontainer = re.sub("_IMAGE_NAME", courseimage_name, runcontainer)
